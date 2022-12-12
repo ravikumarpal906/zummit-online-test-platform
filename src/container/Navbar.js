9e925b2396f170c764dc2393a0279635d9307be1
Ravi Kumar Pal <ravikumarpal906@gmail.com>
2022-12-12 10:41:28 +0530

    navbar

--- src/container/Navbar.js
+++ src/container/Navbar.js
@@ -3,12 +3,21 @@ import React from "react";
 export default function Navbar() {
     return (
       <div className='navbar'>
-          <h1>
-            Hello world
-          </h1>
-          <h2>
-            start web
-          </h2>
+          <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Zummit Online test</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-link active" aria-current="page" href="/">Home</a>
+        <a className="nav-link" href="/">Student</a>
+        <a className="nav-link" href="/">Teacher</a>
+      </div>
+    </div>
+  </div>
+</nav>
       </div>
     )
   }
